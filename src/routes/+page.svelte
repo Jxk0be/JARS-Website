dff64679d65222738b3e76fc49e1ef44d9fde0fc
Jake <JakeShoffner27@gmail.com>
2022-09-15 15:23:14 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,11 +10,6 @@
 <Navbar />
 <Hero />
 
-<div class="w-full h-[70vh] bg-red-100 flex flex-col items-center text-center">
-    <h1 class="mt-[5vh] text-[25px] font-semibold mb-5">Announcements</h1>
-    <div class="grid grid-cols-1 w-full gap-y-10">
-        <div class="w-[full] h-[50vh] bg-red-300 mx-[10vw] rounded-xl flex flex-col items-center text-center">
-            <h1 class="mt-4 text-xl">September 15, 2022</h1>
-        </div>
-    </div>
+<div class="w-full h-full bg-red-200 flex flex-col items-center text-center">
+    <h1>CRINGE</h1>
 </div>
