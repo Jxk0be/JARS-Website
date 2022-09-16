559e14ac7c0433694691f617f23a124805414195
Jake <JakeShoffner27@gmail.com>
2022-09-16 16:29:49 -0400

    Update Announcements.svelte

--- src/components/Announcements.svelte
+++ src/components/Announcements.svelte
@@ -1,9 +1,9 @@
 <div class="w-full bg-white flex flex-col items-center text-center mt-[10vh] z-0 mb-[6vh]">
     <h1 class="font-bold text-[27px] md:text-[35px] uppercase">Organization Announcements</h1>
-    <div class="grid grid-cols-1 gap-y-14 md:grid-cols-2 md:px-[4vw] lg:px-[15vw] w-full h-full mt-[4vh] font-['Raleway']">
+    <div class="grid text-white grid-cols-1 gap-y-14 md:grid-cols-2 md:px-[4vw] lg:px-[15vw] w-full h-full mt-[4vh] font-['Raleway']">
 
         <div class="flex flex-col items-center justify-center w-full drop-shadow-lg">
-            <div class="bg-teal-100 w-[350px] h-[400px]">
+            <div class="bg-[#2F2F2F] w-[350px] h-[400px]">
                 <h1 class="mt-5 text-2xl font-semibold">JARS Rebirth 1.0</h1>
                 <h1 class="text-lg font-medium mb-6">September 22, 2022</h1>
                 <div class="mx-3 text-xl font-medium text-start">
@@ -17,13 +17,13 @@
                     </p>
                 </div>
             </div>
-            <div class="bg-teal-200 w-[350px] h-[50px] flex justify-center items-center">
+            <div class="bg-[#1B1B1B] w-[350px] h-[50px] flex justify-center items-center">
                 <h1 class="text-2xl font-bold uppercase">VOLink</h1>
             </div>
         </div>
 
         <div class="flex flex-col items-center justify-center w-full drop-shadow-lg">
-            <div class="bg-teal-100 w-[350px] h-[400px]">
+            <div class="bg-[#2F2F2F] w-[350px] h-[400px]">
                 <h1 class="mt-5 text-2xl font-semibold">JARS Rebirth 1.0</h1>
                 <h1 class="text-lg font-medium mb-6">September 22, 2022</h1>
                 <div class="mx-3 text-xl font-medium text-start">
@@ -37,8 +37,8 @@
                     </p>
                 </div>
             </div>
-            <div class="bg-teal-200 w-[350px] h-[50px] flex justify-center items-center">
-                <h1 class="text-2xl font-bold uppercase">VOLink</h1>
+            <div class="bg-[#1B1B1B] w-[350px] h-[50px] flex justify-center items-center">
+                <a href="https://volink.utk.edu/organization/jars" target="_blank" class="text-2xl font-bold uppercase transition ease-in-out duration-250 hover:text-[#dd2020]">VOLink</a>
             </div>
         </div>
     </div>
