7c99bbe8338f36835f30282db71a37950db4120d
Jake <JakeShoffner27@gmail.com>
2022-09-16 13:29:56 -0400

    added the Fotter.svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,3 @@
+<div class="flex text-[#000000]/50 text-sm justify-center items-center bg-teal-300 w-full h-[13vh] text-center">
+    <h1 class="mx-3">Fabricated by <a target="_blank" class="underline" href="https://github.com/Jxk0be">Jake Shoffner</a> in the basement of Strong Hall.</h1>
+</div>
