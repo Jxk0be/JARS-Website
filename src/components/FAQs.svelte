475cb87e84bed4005742fdcaa621d4e68ecc2cda
Jake <JakeShoffner27@gmail.com>
2022-10-18 21:15:43 -0400

    Update FAQs.svelte

--- src/components/FAQs.svelte
+++ src/components/FAQs.svelte
@@ -2,49 +2,26 @@
     <h1 class="font-bold text-[27px] md:text-[35px] uppercase mt-[10vh] text-black text-center">FAQs</h1>
     <div class="grid grid-cols-1 gap-y-14 w-[350px] h-full mt-[3vh] font-['Raleway']">
         
-        <div class="bg-[#36393e] flex flex-col items-center md:hover:bg-[#282b30] duration-200 transition ease-in-out w-full h-[45vh] drop-shadow-[-5px_15px_15px_rgba(0,0,0,0.45)]">
-            <h1 class="mt-5 text-2xl font-semibold px-16">JARS Rebirth 1.0</h1>
-            <h1 class="text-lg font-medium mb-6">October 25, 2022</h1>
+        <div class="bg-[#36393e] flex flex-col items-center md:hover:bg-[#282b30] duration-200 transition ease-in-out w-full h-[32vh] drop-shadow-[-5px_15px_15px_rgba(0,0,0,0.45)]">
+            <h1 class="mt-5 text-2xl font-semibold px-16 mb-6">Is this club dead?</h1>
             <div class="mx-5 text-xl font-medium text-start">
                 <p class="mb-3">
-                    Come join us in Strong B1 for an introductory meeting where we 
-                    want to meet everyone and talk about what is to come.
-                </p>
-                <p class="mb-3">
-                    Come join us in Strong B1 for an introductory meeting where we 
-                    want to meet everyone and talk about what is to come.
+                    Unfortunately, no. We were once dead, but we have revived. During
+                    Covid, we were basically non-existent. But with that in the past,
+                    we have now rebirthed and meet weekly in-person.
                 </p>
             </div>
         </div>
 
-        <div class="bg-[#36393e] flex flex-col items-center md:hover:bg-[#282b30] duration-200 transition ease-in-out w-full h-[45vh] drop-shadow-[-5px_15px_15px_rgba(0,0,0,0.45)]">
-            <h1 class="mt-5 text-2xl font-semibold px-16">JARS Rebirth 1.0</h1>
-            <h1 class="text-lg font-medium mb-6">October 25, 2022</h1>
+        <div class="bg-[#36393e] flex flex-col items-center md:hover:bg-[#282b30] duration-200 transition ease-in-out w-full h-[32vh] drop-shadow-[-5px_15px_15px_rgba(0,0,0,0.45)]">
+            <h1 class="mt-5 text-2xl font-semibold px-16 mb-6 text-center">When and where do we meet?</h1>
             <div class="mx-5 text-xl font-medium text-start">
                 <p class="mb-3">
-                    Come join us in Strong B1 for an introductory meeting where we 
-                    want to meet everyone and talk about what is to come.
-                </p>
-                <p class="mb-3">
-                    Come join us in Strong B1 for an introductory meeting where we 
-                    want to meet everyone and talk about what is to come.
+                    Unfortunately, 
                 </p>
             </div>
         </div>
-
-        <div class="bg-[#36393e] flex flex-col items-center md:hover:bg-[#282b30] duration-200 transition ease-in-out w-full h-[45vh] drop-shadow-[-5px_15px_15px_rgba(0,0,0,0.45)]">
-            <h1 class="mt-5 text-2xl font-semibold px-16">JARS Rebirth 1.0</h1>
-            <h1 class="text-lg font-medium mb-6">October 25, 2022</h1>
-            <div class="mx-5 text-xl font-medium text-start">
-                <p class="mb-3">
-                    Come join us in Strong B1 for an introductory meeting where we 
-                    want to meet everyone and talk about what is to come.
-                </p>
-                <p class="mb-3">
-                    Come join us in Strong B1 for an introductory meeting where we 
-                    want to meet everyone and talk about what is to come.
-                </p>
-            </div>
-        </div>
+        <!-- 4711793 -->
     </div>
 </div>
+=
